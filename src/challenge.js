46565c03874f8a119d27041be5bed6393cd6468f
Joakim <joakimtangnes@gmail.com>
2016-10-16 03:29:10 +0200

    Minor fixes

--- src/challenge.js
+++ src/challenge.js
@@ -6,6 +6,8 @@ var rlm_leaderboard = function(options,cb){
 	var apikey_p = options.apikey   || _globals.apikey   || cb("ERROR: No apikey given");
 	var realm_p  = options.realm    || _globals.realm    || cb("ERROR: No realm given");
 
+	if (typeof options == "function") cb = options;
+
 	var baseUrl = "https://:region:.api.battle.net/wow/challenge/:realm:?locale=en_GB&apikey=:apikey:";
 	var url = baseUrl
 		.replace(":realm:",realm_p)
@@ -17,6 +19,8 @@ var rlm_leaderboard = function(options,cb){
 var rgn_leaderboard = function(options,cb){
 	var apikey_p = options.apikey   || _globals.apikey   || cb("ERROR: No apikey given");
 	var region_p = options.region   || _globals.region   || "EU";
+
+	if (typeof options == "function") cb = options;
 
 	var baseUrl = "https://:region:.api.battle.net/wow/challenge/region?locale=en_GB&apikey=:apikey:";
 	var url = baseUrl.replace(":region:",region_p).replace(":apikey:",apikey_p);
